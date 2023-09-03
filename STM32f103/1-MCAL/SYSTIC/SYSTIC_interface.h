375be64802645c803cba6314871beeab20168832
Eslam Nasr <128245776+eslam276@users.noreply.github.com>
2023-09-03 23:25:33 +0300

    Update SYSTIC_interface.h

--- STM32f103/1-MCAL/SYSTIC/SYSTIC_interface.h
+++ STM32f103/1-MCAL/SYSTIC/SYSTIC_interface.h
@@ -8,8 +8,8 @@
 #define AHB_CLOCK           8u   /* IN MHz */
 
 
-uint8_t SYSTIC_delay_ms(uint16_t time);
-uint8_t SYSTIC_delay_us(uint16_t time);
+uint8_t SYSTIC_delay_ms(uint32_t time);
+uint8_t SYSTIC_delay_us(uint32_t time);
 
 
 
